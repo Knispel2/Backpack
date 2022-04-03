c3da0318927514dedbb2e6dedbda2034a855da16
Timofey Pshanichniy <timofei.psheno@gmail.com>
2022-04-03 15:56:17 +0300

    Подгрузка данных + считывание списка файлов

--- Pach.cpp
+++ Pach.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
 #include <vector>
+#include <filesystem>
+using namespace std;
+
+vector <string> list_files(string dir)
+{
+    vector <string> result;
+    for (const auto& entry : filesystem::directory_iterator(dir))
+        result.push_back(entry.path().string().substr(5));
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto data = list_files("data");
+
 }
